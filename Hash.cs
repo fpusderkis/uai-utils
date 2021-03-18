7f38f8c2151580a83993b9e42dfe7130a312ea3c
Federico Pusderkis <federicopusderkis@gmail.com>
2021-03-17 23:54:20 -0300

    Agrego digito verificador y clase de hashs

---
+++ Hash.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uai.utils
+{
+    public class Hash
+    {
+        public static string GenerateMD5(object target)
+        {
+            if (target != null) return ComputeHash(target.ToString(), new MD5CryptoServiceProvider());
+
+            return null;
+        }
+
+
+        public static string GenerateSHA256(object target)
+        {
+            if (target != null) return ComputeHash(target.ToString(), new SHA256CryptoServiceProvider());
+
+            return null;
+        }
+
+        private static string ComputeHash(string input, HashAlgorithm algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
